d4e43f478a6414a119dbf3e26e8d4e71d0d62dd6
AlisonJorre <alison.jorre@isep.fr>
2014-05-05 08:39:37 +0200

    Changes on contact form (mais quel changement ? Je ne sais pas ! :D)

--- raining_music/Controller/contactForm.php
+++ raining_music/Controller/contactForm.php
@@ -1,16 +1,17 @@
 <?php
 include '../model/checkDataBase.php';
 
-$objetContact=$_POST['SubjectChoice'];				//Objet du contact
+$objetContact=$_POST['subjectChoice'];				//Objet du contact
 $emetteur=$_POST['prenomNom'];						//Nom & prenom de l'emetteur
 $adresseEmail=$_POST['emailAddress'];				//Adresse e-mail de l'emetteur
 $numeroDeTel=$_POST['telNum'];						//Numero de telephone de l'emetteur
-$messageComment=n12br($_POST['messageContact']);	//Message
+$messageComment=$_POST['messageContact'];	//Message
 $destinataire="rainingmusic.isep@gmail.com";		//Adresse email de RAINING MUSIC
 
-$messageEtCoordonnees="Nom de l'emetteur du message:".$emetteur.".\n";
+$messageEtCoordonnees="Nom de l'emetteur du message: ".$emetteur.".\nNumero de telephone: ".$numeroDeTel."\nContenu du message:\n\n".$messageComment;
 		
 $headers  = 'From: <' . $adresseEmail .'>' . "\n" ;
 $headers .= 'Content-Type: text/html; charset=iso-8859-15' ;
-mail ($destinataire, $objetContact, $messageComment, $headers) ;
+//mail ($destinataire, $objetContact, $messageEtCoordonnees, $headers) ;
+mail("rainingmusic.isep@gmail.com","bonjour, ceci est un test", "ceci est un header de test, le message est le suivant: ")
 ?>
