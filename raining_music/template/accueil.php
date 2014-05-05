a092d902e8e61bb83e395fb80f9c42d90e82bd46
li <li.zhang81@isep.fr>
2014-05-05 09:44:45 +0200

    defilement manuel de slideshow

--- raining_music/template/accueil.php
+++ raining_music/template/accueil.php
@@ -54,8 +54,24 @@ include("./../layout/basic_header.php");
 	<p class="text">
 		<big><big><strong>Revivez les moments emotionnels</strong> </big> </big>
 	</p>
+
+	<span id="sl_play" class="sl_command">&nbsp;</span>
+	<span id="sl_pause" class="sl_command">&nbsp;</span>
+	<span id="sl_i1" class="sl_command sl_i">&nbsp;</span>
+	<span id="sl_i2" class="sl_command sl_i">&nbsp;</span>
+	<span id="sl_i3" class="sl_command sl_i">&nbsp;</span>
+	<span id="sl_i4" class="sl_command sl_i">&nbsp;</span>
 	
-		<section id="slideshow">
+<section id="slideshow">
+
+		<a class="commands prev commands1" href="#sl_i4" title="Go to last slide">&lt;</a>
+		<a class="commands next commands1" href="#sl_i2" title="Go to 2nd slide">&gt;</a>
+		<a class="commands prev commands2" href="#sl_i1" title="Go to 1rst slide">&lt;</a>
+		<a class="commands next commands2" href="#sl_i3" title="Go to 3rd slide">&gt;</a>
+		<a class="commands prev commands3" href="#sl_i2" title="Go to 2nd slide">&lt;</a>
+		<a class="commands next commands3" href="#sl_i4" title="Go to 4th slide">&gt;</a>
+		<a class="commands prev commands4" href="#sl_i3" title="Go to 3rd slide">&lt;</a>
+		<a class="commands next commands4" href="#sl_i1" title="Go to first slide">&gt;</a>
 
 			<div class="container">
 				<div class="c_slider"></div>
@@ -86,6 +102,13 @@ include("./../layout/basic_header.php");
 			</div>
 
 			<span id="timeline"></span>
+<ul class="dots_commands"><!--
+			--><li><a title="Show slide 1" href="#sl_i1">Slide 1</a></li><!--
+			--><li><a title="Show slide 2" href="#sl_i2">Slide 2</a></li><!--
+			--><li><a title="Show slide 3" href="#sl_i3">Slide 3</a></li><!--
+			--><li><a title="Show slide 4" href="#sl_i4">Slide 4</a></li>
+		</ul>
+			
 
 		</section>
 
