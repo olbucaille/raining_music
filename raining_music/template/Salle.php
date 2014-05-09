8fe08d8f49de933e9de29a337cb9d84721fff25a
ikoroben <ikoroben@serin.isep.fr>
2014-05-09 09:50:19 +0200

    m

--- raining_music/template/Salle.php
+++ raining_music/template/Salle.php
@@ -27,7 +27,7 @@ include("./../layout/basic_header.php");
 <br />
 <br />
 
-<?
+<?php
 if(isset($_POST['requete']) && $_POST['requete'] != NULL) // on vérifie d'abord l'existence du POST et aussi si la requete n'est pas vide.
 {
 mysql_connect('localhost','root','');
@@ -40,17 +40,17 @@ if($nb_resultats != 0) // si le nombre de résultats est supérieur à 0, on con
 // maintenant, on va afficher les résultats et la page qui les donne ainsi que leur nombre
 ?>
 <h3>Résultats de votre recherche.</h3>
-<p>Nous avons trouvé <? echo $nb_resultats; // on affiche le nombre de résultats 
+<p>Nous avons trouvé <?php echo $nb_resultats; // on affiche le nombre de résultats 
 if($nb_resultats > 1) { echo 'résultats'; } else { echo 'résultat'; } // on vérifie le nombre de résultats pour orthographier correctement. 
 ?>
 dans notre base de données. Voici les fonctions que nous avons trouvées :<br/>
 <br/>
-<?
+<?php
 while($donnees = mysql_fetch_array($query)) // on fait un while pour afficher la liste des fonctions trouvées, ainsi que l'id qui permettra de faire le lien vers la page de la fonction
 {
 ?>
-<a href="fonction.php?id=<? echo $donnees['id']; ?>"><? echo $donnees['nom_fonction']; ?></a><br/>
-<?
+<a href="fonction.php?id=<?php echo $donnees['id']; ?>"><?php echo $donnees['nom_fonction']; ?></a><br/>
+<?php
 } // fin de la boucle
 ?><br/>
 <br/>
