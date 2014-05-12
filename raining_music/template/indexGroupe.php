3f3d7d80d7b6f94ca33a59b150dd775be4badf33
Farahbm <farah.benmohamed@isep.com>
2014-05-12 09:55:14 +0200

    index groupe modif

---
+++ raining_music/template/indexGroupe.php
@@ -0,0 +1,55 @@
+
+
+<?php 
+include("./../layout/basic_header.php");
+?>
+
+
+<!--Allez vers la liste des artistes suivant le nom!-->
+<!-- envoyer la lettre comme parametre en URL  -->
+ <a href="Redirection.php?lettre=a"> A </a>
+ <a href="Redirection.php?lettre=b"> B </a> 
+ <a href="Redirection.php?lettre=c"> C </a> 
+ <a href="Redirection.php?lettre=d"> D </a> 
+ <a href="Redirection.php?lettre=e"> E </a> 
+ <a href="Redirection.php?lettre=f"> F </a> 
+ <a href="Redirection.php?lettre=g"> G </a> 
+ <a href="Redirection.php?lettre=h"> H </a> 
+ <a href="Redirection.php?lettre=i"> I </a> 
+ <a href="Redirection.php?lettre=j"> J </a> 
+ <a href="Redirection.php?lettre=k"> K </a> 
+ <a href="Redirection.php?lettre=l"> L </a> 
+ <a href="Redirection.php?lettre=m"> M </a> 
+ <a href="Redirection.php?lettre=n"> N </a> 
+ <a href="Redirection.php?lettre=o"> O </a> 
+ <a href="Redirection.php?lettre=p"> P </a> 
+ <a href="Redirection.php?lettre=q"> Q </a> 
+ <a href="Redirection.php?lettre=r"> R </a> 
+ <a href="Redirection.php?lettre=s"> S </a> 
+ <a href="Redirection.php?lettre=t"> T </a> 
+ <a href="Redirection.php?lettre=u"> U </a> 
+ <a href="Redirection.php?lettre=v"> V </a> 
+ <a href="Redirection.php?lettre=w"> W </a> 
+ <a href="Redirection.php?lettre=x"> X </a> 
+ <a href="Redirection.php?lettre=y"> Y </a> 
+ <a href="Redirection.php?lettre=z"> Z </a> 
+ <a href="#.html"> # </a> 
+
+<br />
+<br />
+<!-- Rechercher un groupe -->
+		 <div class="search">
+				
+				<form  class="search" method="post" action="Redirection.php">
+			
+				<input class="search_data" name="saisie" type="search" placeholder="Tapez le nom du groupe" required />
+				<input class="btn-right-loupe" name="go" type="submit" />
+				</form></div>
+				
+				<marquee>dp4.jpg</marquee>
+				
+				<?php 
+	include("./../layout/basic_footer.php");
+	?>
+            
+            
