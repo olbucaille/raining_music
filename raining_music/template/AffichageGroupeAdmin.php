26008bae7107b47ad9189e6d9289416a37d267c2
Farahbm <farah.benmohamed@isep.com>
2014-05-19 20:47:07 +0200

    Changement de la page AffichageGroupe.php en AffochageGroupeAdmin.php qui est donc la pageadmin du groupe et qui comprend dans un premier temps la modification de la photo de profil de groupe

--- raining_music/template/AffichageGroupeAdmin.php
+++ raining_music/template/AffichageGroupeAdmin.php
@@ -4,15 +4,28 @@ include("./../layout/basic_header.php");
 <!-- debut de la page en elle meme-->
 </header>
 <div class="conteneur" style="margin-left:5%; width:90%; min-height:500px; height:100%; background-color:#c0c0c0; ">
-	
-
-   
-<p> 
+	<p> 
 <center><font size = 18> DAFT PUNK </font></center>
+<?php
+$name=$_GET['id_groupe']."_groupe.JPG";
+	/*if (file_exists('upload_pictures/'.$name)){ echo"<img src='img/photos/$name' width='90' height='90' border='2'/>";}
+	else { echo"<img src='img/no_photo.png' width='90' height='90' border='2'/>";}
+	*/
+echo"<img src='upload_pictures/$name' alt='DP' border=':#0b8dca thick solid' height='200' width='250' style='position:relative;top:5px; margin-right:10px ; margin-bottom: 15px;'  />";
+?>
+</p>
 
-<img src="./../pictures/DP3.jpeg" alt="DP" border=":#0b8dca thick solid" height="200" width="250" style="position:relative;top:5px; margin-right:10px ; margin-bottom: 15px;"  /></p>
+
+<!-- télécharger une nouvelle photo pour le groupe -->
+<form action="imageUpload.php" method="post" enctype="multipart/form-data"  target="hiddeniframe" >
+   <input type="hidden" name="id_groupe" value="<?php echo $_GET['id_groupe']; ?>"> 
+    <input type="file" name="imgfile" /> 
+    <input type='submit' name="uploadButton" value="Changer la photo" />
+	
+</form>
 
     <div class="left" style="  padding-left:10px;">
+    	
     	<p>
     	<img src="./../pictures/playlist1.jpeg" alt=" " border=":#0b8dca thick solid" height="50" width="200" style="position:relative;top:15px; margin-right:5px ; margin-bottom:15px;"  /></p>
    		<font size = 5><font color="black">Give life back to music</font></font> <audio src = "Give life back to music.mp3" controls width=10></audio>
