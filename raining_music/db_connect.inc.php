e3674295d52819e2fd3ace093fb325aa84a13679
IrinaKorobenko <irina.korobenko@isep.fr>
2014-05-24 22:12:39 +0200

    Nouvelle bd

--- raining_music/db_connect.inc.php
+++ raining_music/db_connect.inc.php
@@ -6,7 +6,7 @@ function connect(){
 
 	$PARAM_hote='10.0.120.16'; // le chemin vers le serveur
 	$PARAM_port='3306';
-	$PARAM_nom_bd='dotgamehmod1'; // le nom de votre base de données
+	$PARAM_nom_bd='bd_raining_music'; // le nom de votre base de données
 	$PARAM_utilisateur='dotgamehmod1'; // nom d'utilisateur pour se connecter
 	$PARAM_mot_passe='UOA6up7E'; // mot de passe de l'utilisateur pour se connecter
 		$connexion =  new PDO('mysql:host='.$PARAM_hote.';port='.$PARAM_port.';dbname='.$PARAM_nom_bd, $PARAM_utilisateur, $PARAM_mot_passe);
