8a5649be0aecbfa863c256db2e216a612574f2b5
IrinaKorobenko <irina.korobenko@isep.fr>
2014-05-27 18:26:40 +0200

    Modifs salle

--- raining_music/index.php
+++ raining_music/index.php
@@ -32,19 +32,11 @@ switch ((string)$action) {
 	case '\'upload_music\'':
 		c_UploadMusic();
 		break;
-	
-		
 	case '\'refuser_adhesion_membre_groupe\'':
 		c_RefuserAdhsionGroupe();
 		break;
 	case '\'accepter_adhesion_membre_groupe\'':
 		c_AccepterAdhesionGroupe();
-		break;
-	case '\'refuser_adhesion_salle\'':
-		c_RefuserAdhsionSalle();
-		break;
-	case '\'accepter_adhesion_salle\'':
-		c_AccepterAdhesionSalle();
 		break;
 	default :
 		header("location:./template/accueil.php");
