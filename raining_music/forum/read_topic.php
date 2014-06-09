41009a88b499a336b31e68ae36acdaeeae46c71c
olbucaille <olivier.bucaille@isep.fr>
2014-06-09 13:59:18 +0200

    correctif pour le forum :
    accents gérés
    message tout moche d'accueil
    images de la colone "action" chargé correctement

--- raining_music/forum/read_topic.php
+++ raining_music/forum/read_topic.php
@@ -75,22 +75,6 @@ if(isset($_SESSION['username']))
 	<a href="new_reply.php?id=<?php echo $id; ?>" class="button">Reply</a>
 <?php
 }
-else
-{
-?>
-<div class="box_login">
-	<form action="login.php" method="post">
-		<label for="username">Username</label><input type="text" name="username" id="username" /><br />
-		<label for="password">Password</label><input type="password" name="password" id="password" /><br />
-        <label for="memorize">Remember</label><input type="checkbox" name="memorize" id="memorize" value="yes" />
-        <div class="center">
-	        <input type="submit" value="Login" /> <input type="button" onclick="javascript:document.location='signup.php';" value="Sign Up" />
-        </div>
-    </form>
-</div>
-<?php
-}
-
 include'./../layout/basic_footer.php';
 }
 else
