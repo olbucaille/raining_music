240293b78d8c9c1dd7d59db04bf35ee94c577f79
AlisonJorre <alison.jorre@isep.fr>
2014-06-13 11:52:45 +0200

    IRINA => Ajout de la redirection de salle

--- raining_music/layout/menu.php
+++ raining_music/layout/menu.php
@@ -21,7 +21,7 @@ if(isset($_SESSION['user']))
 </ul>
 </li>
 <li><a class="menu" href="./../template/Redirection.php" title="Artistes">Les artistes</a></li>
-<li><a  class="menu" href="../TEST/buildingInProgress.php" title="SallesConcert">Les salles de concert</a></li>
+<li><a  class="menu" href="./../template/RedirectionSalle.php" title="SallesConcert">Les salles de concert</a></li>
 
 
 <li><a class="menu" href="./../template/RechercheAvancee.php" title="Recherche avancee">Recherche avancée</a></li>
